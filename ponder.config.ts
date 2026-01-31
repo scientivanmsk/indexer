acf8ead1989bd50c67b104689eb85a8cfa2379bb
aevryiam <ilhamyusufwiam@mail.ugm.ac.id>
2026-01-31 20:10:24 +0700

    fix: update address

--- ponder.config.ts
+++ ponder.config.ts
@@ -15,13 +15,13 @@ export default createConfig({
     Campaign: {
       abi: CampaignAbi,
       chain: "baseSepolia",
-      address: "0x44e87aa98d721Dbcf368690bF5aAb1F3dD944dA9",
+      address: "0x17fb0DD846d2299F525ca0d0402C607C580e80c8",
       startBlock: 36692000,
     },
     Badge: {
       abi: BadgeAbi,
       chain: "baseSepolia",
-      address: "0xaE32Df9Fb677aE68C5A1F956761a42e269Ebdc99",
+      address: "0x8bdfD4C3f8e108687ABA5d9ebD9aFFe355545471",
       startBlock: 36692000,
     },
   },
